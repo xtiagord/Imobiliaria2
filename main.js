2b6bd2143ce878d92d1a715df227f2a1b8c6ffb1
Tiago Dalmolin <tiagodalmolin028@gmail.com>
2023-06-22 04:00:35 -0300

    Web Brokers

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-let search = document.querySelector('.search-box')
+let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    menu.classList.remove('active');
 }
+
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+//hide menu an search box on scroll
+
+window.onscroll = () =>{
+    menu.classList.remove('active');
+    search.classList.remove('active');
+}
+//Header
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
